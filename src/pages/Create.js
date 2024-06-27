e9867958a6b119bc52c4924efd07d7235c83e067
GripAll <96963277+GripAll@users.noreply.github.com>
2024-06-27 23:25:47 +0200

    yes

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
 
 export default function Create() {
   return (
     <div>
-      Create page
+
+      <Typography variant='h1'>
+        Create a new note
+      </Typography>
     </div>
   )
 }
